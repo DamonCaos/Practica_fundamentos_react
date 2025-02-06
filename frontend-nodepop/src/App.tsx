63e1d0b214f0b60c865977425126d710b283e35a
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-02-06 20:05:40 +0100

    Estructurando el proyecto, instalando librerias y creando archivos principales

--- frontend-nodepop/src/App.tsx
+++ frontend-nodepop/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import AdvertsPage from "./pages/AdvertsPage";
+import DetailAdvertPage from "./pages/DetailAdvertPage";
+import NewAdvertPage from "./pages/NewAdvertPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/adverts" element={<AdvertsPage />} />
+        <Route path="/advert/new" element={<NewAdvertPage />} />
+        <Route path="/advert/:id" element={<DetailAdvertPage />} />
+        <Route path="/" element={<Navigate to="/adverts" />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
