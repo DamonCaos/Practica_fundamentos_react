effbe475bbc24d3f4893731e38737496d97c2281
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-03-05 21:17:52 +0100

    Envolvemos app con el provider de redux para que todos los componentes tengan acceso a la store

--- frontend-nodepop/src/main.tsx
+++ frontend-nodepop/src/main.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"; 
+import store from "./redux/store"; 
 import { AuthProvider } from "./context/AuthContext";
-import { NotificationProvider } from "./context/NotificationContext"; // 🟢 Importar el NotificationProvider
+import { NotificationProvider } from "./context/NotificationContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <NotificationProvider> 
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </NotificationProvider>
-    </BrowserRouter>
+    <Provider store={store}> {/* 🔹 Envolvemos la app con el Provider de Redux */}
+      <BrowserRouter>
+        <NotificationProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </NotificationProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
